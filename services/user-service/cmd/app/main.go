93c3b5eb9ff3d7e4d36ea71a9a47ae1ed3fab027
Konstantin Iakovlev <yakonstantine@gmail.com>
2026-03-02 12:26:13 +0100

    Docker configuration

--- services/user-service/cmd/app/main.go
+++ services/user-service/cmd/app/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		fmt.Printf("%v: heartbeat\n", time.Now().Format(time.RFC3339))
+	}
+}
